37e1007b6396b634b96f4070b70c96f45101fe15
tegu1365 <tedi.d.petkova@gmail.com>
2023-02-04 20:41:56 +0200

    User login,logout & registration done

---
+++ TropeBingo/base/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name",
+                  "password1", "password2")
