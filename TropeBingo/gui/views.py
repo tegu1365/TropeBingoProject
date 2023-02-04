cff36de75b14d62a27280a15af308a44459052ac
tegu1365 <tedi.d.petkova@gmail.com>
2023-02-04 20:42:42 +0200

    Fails that I forgot

---
+++ TropeBingo/gui/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+from base.forms import RegisterUserForm
+
+
+def index(request):
+    """Welcome page."""
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(index)
+    else:
+        form = RegisterUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/registration.html', context)
+
+
+@login_required(login_url='/login')
+def default(request):
+    return render(request, 'default.html')
