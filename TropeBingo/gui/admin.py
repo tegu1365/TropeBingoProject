ff123ebdf95d35072b725a206ec47d428f9f853f
tegu1365 <tedi.d.petkova@gmail.com>
2023-02-10 15:10:11 +0200

    Added models and admin panels for Bingo and Tropes

--- TropeBingo/gui/admin.py
+++ TropeBingo/gui/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Trope, BingoSheet, Genre
 
-# Register your models here.
+
+class GenreInline(admin.TabularInline):
+    model = Trope.genres.through
+    extra = 1
+
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+    ordering = ('name',)
+    search_fields = ('name',)
+    list_display = ('name',)
+    fields = ('name',)
+
+
+class TropeAdmin(admin.ModelAdmin):
+    model = Trope
+    ordering = ('name',)
+    search_fields = ('name',)
+    filter_horizontal = ('genres',)
+    list_display = ('name', 'description','display_genres')
+    fields = ('name', 'description')
+    inlines = [GenreInline]
+
+    def display_genres(self, obj):
+        return ', '.join([genre.name for genre in obj.genres.all()])
+
+    display_genres.short_description = 'Genres'
+
+
+class BingoSheetAdmin(admin.ModelAdmin):
+    model = BingoSheet
+    ordering = ('name', 'owner')
+    search_fields = ('name',)
+    list_display = ('name', 'owner', 'code', 'checked', 'private')
+    fields = ('name', 'owner', 'code', 'checked', 'private')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Trope, TropeAdmin)
+admin.site.register(BingoSheet, BingoSheetAdmin)
--- TropeBingo/gui/models.py
+++ TropeBingo/gui/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Trope(models.Model):
+    """This is the model for the bingo elements that are tropes"""
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    genres = models.ManyToManyField(Genre, related_name='tropes')
+
+    def __str__(self):
+        return self.name + "-" + self.description
+
+
+class BingoSheet(models.Model):
+    """Model for the bingo sheet"""
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=255)  # we can't save the bingo as bingo in the database so I created a coding
+    # system for this (similar to the s one )
+    checked = models.CharField(max_length=25)  # witch fields are marked
+    private = models.BooleanField(default=False)  # for future use when i have a friend list implemented
+
+    def __str__(self):
+        return self.name + "( " + self.code + ")"
